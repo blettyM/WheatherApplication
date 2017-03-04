261c8ef23207e30cd5abc102d34b71bed67d3b40
Bletty Jans <BlettyJans@Blettys-MacBook-Air.local>
2017-03-04 12:37:23 -0500

    Initial Commit

---
+++ WheatherApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WheatherApp
+//
+//  Created by Bletty Jans on 04/03/17.
+//  Copyright © 2017 cs2680. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
